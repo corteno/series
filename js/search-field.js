5d439b7c660f9a12951061c7705e302d8bb84d59
tomi <tomi>
2017-02-09 16:39:44 +0100

    - Search bar - CSS

---
+++ js/search-field.js
@@ -0,0 +1,25 @@
+var searchField = document.getElementById("search-field");
+var label = document.getElementById("search-label");
+
+label.addEventListener("click", () => {
+    if(!hasClass(searchField, "search-field-open")){
+        searchField.className += " search-field-open";
+        searchField.focus();
+    } else {
+        searchField.className = "search-field";
+    }
+
+
+
+});
+
+searchField.addEventListener("blur", () => {
+    if(hasClass(searchField, "search-field-open")){
+        searchField.className = "search-field";
+    }
+
+});
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
